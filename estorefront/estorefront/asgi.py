f450e9c0dd6bc42346a5937f983c0502d2ca2660
darthkaan <darthkaan@localhost.localdomain>
2024-01-07 14:10:45 -0400

    changed the folder name and thus had to change internal stuff too

--- estorefront/estorefront/asgi.py
+++ estorefront/estorefront/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for intelligentreflections project.
+ASGI config for estorefront project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intelligentreflections.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estorefront.settings')
 
 application = get_asgi_application()
--- estorefront/estorefront/wsgi.py
+++ estorefront/estorefront/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for intelligentreflections project.
+WSGI config for estorefront project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intelligentreflections.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estorefront.settings')
 
 application = get_wsgi_application()
